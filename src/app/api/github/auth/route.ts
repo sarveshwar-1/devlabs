ea433b8aba66803254b34a17532a7f364634722d
Sarveshwar B <sarveshwar2005@gmail.com>
2025-01-23 00:12:32 +0530

    pdate user model to enforce unique roll number, add registration API, and enhance GitHub auth error handling

--- src/app/api/github/auth/route.ts
+++ src/app/api/github/auth/route.ts
@@ -1,26 +1,25 @@
-
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/lib/auth';
 
 export async function GET(req: NextRequest) {
     try {
-        
         const session = await auth();
         if (!session?.user?.email) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
+        
         console.log('redirect uri', `${process.env.NEXT_PUBLIC_APP_URL}/api/github/callback`);
     
         const params = new URLSearchParams({
             client_id: process.env.GITHUB_CLIENT_ID!,
-            // redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}/api/github/callback`,
             scope: 'read:user user:email repo',
-            state: session.user.email, // Use email as state to verify user
+            state: session.user.email,
         });
     
         const url = `https://github.com/login/oauth/authorize?${params}`;
         return NextResponse.json({ url });
-    }catch(error){
+    } catch(error) {
         console.error("github auth error", error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 }
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/db/prismadb";
+
+export async function POST(req: NextRequest) {
+    const { name,email, password,rollNo,role  } = await req.json();
+  if (!email || !password) {
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  }
+
+  try {
+    const user = await prisma.user.create({
+        data: {
+            name,
+        email,
+        password,
+        rollNo,
+            role,
+      },
+    });
+    if (role === "MENTOR") {
+      await prisma.mentor.create({
+        data: {
+          id: user.id,
+        },
+      });
+    }
+    if (role === "MENTEE") {
+      await prisma.mentee.create({
+        data: {
+          id: user.id,
+        },
+      });
+    }
+    if (role === "ADMIN") {
+      await prisma.admin.create({
+        data: {
+          id: user.id,
+        },
+      });
+    }
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("Register error:", error);
+    return NextResponse.json({ error: "Register error" }, { status: 500 });
+  }
+}
